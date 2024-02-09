0f3f647cd2b0683fe3aabc4d1b8e5d68614249da
Bladimir Arroyo <bladimir.ab@gmail.com>
2024-02-08 19:23:09 -0600

    Using the querystring on week-1 example

--- weeks/week-1/server/index.js
+++ weeks/week-1/server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
+
 //middlewares
 app.use(bodyParser.json());
 app.use(cors({
@@ -12,12 +13,31 @@ app.use(cors({
 
 //routes
 app.get('/tipocambio', function (req, res) {
-  res.json({
-    "TipoCompraDolares" : "621",
-    "TipoVentaDolares" : "621",
-    "TipoCompraEuros" : "731.85",
-    "TipoVentaEuros" : "761.9"
-  });
+  let response = {};
+  switch(req.query.type) {
+    case 'usd':
+      response = {
+        "TipoCompraDolares" : "621",
+        "TipoVentaDolares" : "621"
+      }
+    break;
+    case 'eur':
+      response = {
+        "TipoCompraEuros" : "731.85",
+        "TipoVentaEuros" : "761.9"
+      }
+    break;
+    default:
+      response = {
+        "TipoCompraDolares" : "621",
+        "TipoVentaDolares" : "621",
+        "TipoCompraEuros" : "731.85",
+        "TipoVentaEuros" : "761.9"
+      }
+    break;
+  }
+  res.json(response);
 });
+
 //start the app
 app.listen(3001, () => console.log(`BBCR Exchange type service listening on port 3001!`))
