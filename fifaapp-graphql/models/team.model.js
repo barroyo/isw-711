1d2db4efba4b2fa9f0c1ed0b12fb0fb088270697
Bladimir Arroyo <bladimir.ab@gmail.com>
2026-01-16 16:02:19 -0600

    Repo cleanup

--- fifaapp-graphql/models/team.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-
-export const teamSchema = new Schema({
-  name: { type: String },
-  description: { type: String }
-});
-export const teamModel = mongoose.model('teams', teamSchema);
